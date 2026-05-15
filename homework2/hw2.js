dbcd09afbd5db3de589b116ed8340b557c576d9c
YoungSun <sun010331@naver.com>
2026-05-15 19:17:57 +0900

    feat : 9주차

--- homework2/hw2.js
+++ homework2/hw2.js
@@ -1,3 +1,23 @@
+function displayToday() {
+    const now = new Date(); // 현재 날짜와 시간 가져오기
+
+    const year = now.getFullYear(); // 년
+    
+
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    
+
+    const day = String(now.getDate()).padStart(2, '0');
+
+
+    const formattedDate = `${year}/${month}/${day}`;
+
+
+    document.getElementById('today-date').textContent = formattedDate;
+}
+
+displayToday();
+
 function t() {
     alert("작동?");
 }
